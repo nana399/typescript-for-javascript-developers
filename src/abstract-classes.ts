c6c567d58e3fab606245801f0e64cff798cb0ba1
Nana Adachi <2022dip-student002@la-study.com>
2022-07-12 15:40:56 +0900

    [add]return-of-interfaces.ts

---
+++ src/abstract-classes.ts
@@ -0,0 +1,18 @@
+export {};
+
+//親クラス
+abstract class Animal {
+  abstract cry(): string;
+}
+
+class Lion extends Animal {
+  cry(){
+    return 'roar';
+  }
+}
+
+class Tiger extends Animal {
+  cry(){
+    return 'grrrrrrr';
+  }
+}
---
+++ src/return-of-interfaces.ts
@@ -0,0 +1,28 @@
+export {};
+
+// class Mahotsukai {}
+// class Souryo {}
+
+// class Taro extends Mahotsukai {}
+
+// interface kenja {
+//   ionazun(): void;
+// }
+
+// interface Senshi {
+//   kougeki(): void;
+// }
+
+// class Jiro inplements Kenja, Senshi {
+//   ionazun(): void{
+//     console.log('iozazun');
+//   }
+
+//   kougeki(): void{
+//     console.log('kougeki');
+//   }
+// }
+
+// const jiro = new Jiro();
+// jiro.ionazun();
+// jiro.kougeki();
